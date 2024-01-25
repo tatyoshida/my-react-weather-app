ae988ef394a5eb8e8ca05dc4973d5991ba594c67
Tatiana Yoshida <tatyoshida@gmail.com>
2024-01-25 00:08:12 -0300

    Footer updated

--- src/Footer.js
+++ src/Footer.js
@@ -4,9 +4,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="Footer">
-      <p>
-        Coded by <em>Tatyoshida</em>
-      </p>
+      This project was coded by <a href="https://github.com/tatyoshida" target="blank"><em>Tatyoshida</em></a>, is <a href="https://github.com/tatyoshida/my-react-weather-app" target="blank">open-sorced on GitHub</a> and <a href="https://delightful-chaja-a5a2c2.netlify.app/" target="blank">hosted on Netlify</a>.
     </div>
   );
 }
