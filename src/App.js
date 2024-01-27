caa6ae6626c8e467662a035200d2902eaabc0e3b
Tatiana Yoshida <tatyoshida@gmail.com>
2024-01-27 14:24:56 -0300

    Setup structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import Header from "./Header";
-import Footer from "./Footer";
-import Weather from "./Weather";
+
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Weather city="Ituzaingó" day="Sábado" hour={14} minutes={50} />
-      <Footer />
+      Weather App
     </div>
   );
 }
